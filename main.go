d4e866e7abc330459765150e9b1607e584252d94
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-08-06 18:40:07 +0700

    learn path and query string

--- main.go
+++ main.go
@@ -9,7 +9,23 @@ func main(){
 	r := gin.Default()
 	r.GET("/", helloWorld)
 	r.GET("/data", myData)
+	r.GET("/data/:id", dataSpesific)
+	r.GET("/query", dataQuery)
 	r.Run(":3000")
+}
+func dataQuery(c *gin.Context){
+	name := c.Query("name")
+	c.JSON(http.StatusOK, gin.H{
+		"name": name,
+		"message": "tes",
+	})
+}
+
+func dataSpesific(c *gin.Context){
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
+	})
 }
 
 func helloWorld(c *gin.Context){
